cdfa5ad2e93f32a15489b186f80c8a9b71d385bb
Michal Conos <michal.conos@oracle.com>
2018-11-11 11:54:10 +0100

    omit scale from out image

--- examples/eye-enhancer.cpp
+++ examples/eye-enhancer.cpp
@@ -18,9 +18,7 @@ using namespace cv;
 using namespace std;
 
 static Rect face;
-static float scale;
 
-#define SCALE_SIZE 256.0f
 
 static void face_and_eye_detect(char *image_name) 
 {
@@ -35,11 +33,6 @@ static void face_and_eye_detect(char *image_name)
   cvtColor(img, img, COLOR_RGB2GRAY);
   equalizeHist(img, img);
   cerr << img.cols << "x" << img.rows << endl;
-  //scale = img.cols / SCALE_SIZE;
-  //Size size((int)(img.cols/scale),(int)(img.rows/scale));//the dst image size
-  //Mat dst;//dst image
-  //resize(img,dst,size);//resize image
- // cerr << dst.cols << "x" << dst.rows << endl;
   cvNamedWindow( "Image", WINDOW_AUTOSIZE );
   vector<double> weights;
   vector<int> levels;
@@ -57,7 +50,7 @@ static void face_and_eye_detect(char *image_name)
   rectangle(img, detections[0], Scalar(255, 255, 0));
   imshow("Image", img);
   //cerr << "Detection " << detections[0] << " with weight " << weights[0] << endl;
-  cvWaitKey();
+//  cvWaitKey();
   face = detections[0];
 }
 
@@ -65,7 +58,7 @@ void mark_face(char *image_name)
 {
   
   layer_t layer = read_JPEG_file(image_name);
-  rect_t face_zone = {int(face.x*scale), int(face.y*scale), int((face.x+face.width)*scale), int((face.y+face.height)*scale)};
+  rect_t face_zone = {int(face.x), int(face.y), int(face.x+face.width), int(face.y+face.height)};
   draw_rect(layer, vec3_init(1.0f, 1.0f, 0.0f), 1.0f, face_zone, blend_normal);
   write_JPEG_file(string("face.jpg").c_str(), layer, 90);
 }
