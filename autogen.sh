e1f690ee90f19f55f887c3405a2a12ee99769584
Michal Conos <mikc@Michal-MacBook-Pro.local>
2020-01-05 13:42:57 +0100

    raspbian compilation changes

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,17 @@
 OS=$(uname -s)
 
 [ "$OS" = "Linux" ] && {
-  sudo apt install libjpeg-dev
-  sudo apt install libexif-dev
+  sudo apt install -y libjpeg-dev
+  sudo apt install -y libexif-dev
+  sudo apt install -y ffmpeg
+  sudo apt install -y automake
+  sudo apt install -y cmake
+  sudo apt install -y pkg-config
+  sudo apt install -y tesseract-ocr
+  #sudo apt install -y libblas-dev
+  sudo apt install -y libopenblas-dev
+  sudo apt install -y python-dev
+
   sudo apt install libavutil-dev libavcodec-dev
   sudo apt install libavformat-dev
   sudo apt install libavcodec-extra
