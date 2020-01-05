23f3c8ed19901bafdddd38ff26e7271cbbd281f0
Michal Conos <mikc@Michal-MacBook-Pro.local>
2020-01-05 17:36:08 +0100

    support medianmask size from command line

--- examples/median.c
+++ examples/median.c
@@ -7,15 +7,18 @@
 #include <levels.h>
 
 
+#define DEFAULT_MEDIAN_MASK_SZ 3
 
 int main(int argc, char *argv[]) {
+  int mask_size = DEFAULT_MEDIAN_MASK_SZ;
   /**
    * Read the source image
    */
   layer_t source = read_JPEG_file(argv[1]);
   layer_info(source);
+  if(argc>=3) mask_size = atoi(argv[2]);
 
   rect_t adjust_zone = { 0, 0, source.width, source.height };
-  median_filter(source, 5, adjust_zone);
+  median_filter(source, mask_size, adjust_zone);
   write_JPEG_file("median-result.jpg", source, 90);
 }
